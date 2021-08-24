e5a20edf800aa993f19640f90f146a23afc0f837
DeanKamel <odean1@hotmail.com>
2021-08-23 22:56:37 -0500

    Web hosting

--- ExpressViews/urls.py
+++ ExpressViews/urls.py
@@ -28,6 +28,6 @@ urlpatterns = [
     path('members/', include('members.urls')),
 
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    # url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
+    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
